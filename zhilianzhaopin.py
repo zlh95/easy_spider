4f7c71aff66aa06b317b4307a89f8640e2b8c0d9
zhaolehua <346767586@qq.com>
2018-02-05 18:01:07 +0800

    modified:   zhilianzhaopin.py

--- zhilianzhaopin.py
+++ zhilianzhaopin.py
@@ -1,13 +1,17 @@
-import csv
-import re
-from urllib.parse import urlencode
-
-from tqdm import tqdm
-from requests.exceptions import RequestException
 import requests
+from urllib.parse import urlencode
+from bs4 import BeautifulSoup
+import pymongo
+from requests.exceptions import RequestException
+from threading import Thread
+from queue import Queue
 
+q = Queue()
+NUM = 360
+JOBS = 6
 
-
+client = pymongo.MongoClient('localhost')
+db = client['��Ƹ��վ']
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
@@ -18,76 +22,63 @@ headers = {
     'Accept-Language': 'zh-CN,zh;q=0.9'
 }
 
-def get_one_page(city,keyword,region,page):
+def get_one_page(city,keyword,page):
     paras = {
-        'jl': city,  # 搜索城市
-        'kw': keyword,  # 搜索关键词
-        'isadv': 0,  # 是否打开更详细搜索选项
-        'isfilter': 1,  # 是否对结果过滤
-        'p': page,  # 页数
-        're': region  # region的缩写，地区，2005代表海淀
+        'jl': city,
+        'kw': keyword,
+        'sm':'0',
+        'p': page,
     }
-    url = 'https://sou.zhaopin.com/jobs/searchresult.ashx?' + urlencode(paras)
     try:
-        response = requests.get(url,headers=headers)
+        url = 'https://sou.zhaopin.com/jobs/searchresult.ashx?' + urlencode(paras)
+        response = requests.get(url,verify=False,headers=headers)
         if response.status_code == 200:
             return response.text
         return None
     except RequestException:
-        print('解析首页失败')
-        return None
+        print('��ȡ��ҳʧ�ܣ�')
 
 def parse_one_page(html):
-    pattern = re.compile('<a style=.*? target="_blank">(.*?)</a>.*?'
-                         '<td class="gsmc"><a href="(.*?)" target="_blank">(.*?)</a>.*?'
-                         '<td class="zwyx">(.*?)</td>',re.S)
-    items = re.findall(pattern,html)
-    for item in items:
-        job_name = item[0]
-        job_name = job_name.replace('<b>', '')
-        job_name = job_name.replace('</b>', '')
-        yield {
-            '职位': job_name,
-            '公司网站': item[1],
-            '公司名称': item[2],
-            '薪水': item[3]
+    soup = BeautifulSoup(html,'lxml')
+    content = soup.find('div',attrs={'class':'newlist_list_content'})
+    #print(content)
+    company = content.find_all('td',{'class':'gsmc'})
+    companies = (i.text for i in company)
+    salary = content.find_all('td',{'class':'zwyx'})
+    salary_level = (i.text for i in salary)
+    localtion = content.find_all('td',{'class':'gzdd'})
+    localtions = (i.text for i in localtion)
+    position = content.find_all('div',{'style':'width: 224px;*width: 218px; _width:200px; float: left'})
+    positions = (i.text.strip() for i in position)
+    for a,b,c,d in zip(positions,salary_level,companies,localtions):
+        result = {
+            'position':a,
+            'salary_level':b,
+            'company':c,
+            'localtion':d
         }
+        save_to_mongo(result)
 
-def write_csv_file(path, headers, rows):
-    '''
-    将表头和行写入csv文件
-    '''
-    # 加入encoding防止中文写入报错
-    # newline参数防止每写入一行都多一个空行
-    with open(path, 'a', encoding='gb18030', newline='') as f:
-        f_csv = csv.DictWriter(f, headers)
-        f_csv.writeheader()
-        f_csv.writerows(rows)
+def save_to_mongo(result):
+    if db['������Ƹ'].insert(result):
+        print('�ɹ��洢��MongoDB',result)
+        return True
+    return False
 
 
-def write_csv_headers(path,headers):
-    with open(path,'a',encoding='gb18030',newline='') as f:
-        f_csv = csv.DictWriter(f,headers)
-        f_csv.writeheader()
+def main(num):
+    html = get_one_page(city='��ɳ',keyword='python',page=num)
+    parse_one_page(html)
 
-
-def write_csv_rows(path,headers,rows):
-    with open(path,'a',encoding='gb18030',newline='') as f:
-        f_csv = csv.DictWriter(f,headers)
-        f_csv.writerows(rows)
-
-
-
-def main(city,keyword,region,pages):
-    file_name = 'zlzp_' + city + '_' + keyword + '.csv'
-    headers = ['职位', '公司网站', '公司名称', '薪水']
-    write_csv_headers(file_name,headers)
-    for i in tqdm(range(pages)):
-        jobs =[]
-        html = get_one_page(city,keyword,region,i)
-        for item in parse_one_page(html):
-            jobs.append(item)
-        write_csv_rows(file_name,headers,jobs)
-
-if __name__ == '__main__':
-    main('北京','爬虫工程师',2005,30)
+def working():
+    while True:
+        arguments = q.get()
+        main(arguments)
+        q.task_done()
+for i in range(NUM):
+    t = Thread(target=working)
+    t.setDaemon(True)
+    t.start()
+for i in range(JOBS):
+    q.put(i)
+q.join()
